635be5418e26becc6e8e32554a85d8fcc2e89e48
aamir0143 <mdaamirreza00@gmail.com>
2022-01-24 23:51:28 +0530

    [Aamir3] Add : Added Calculate Wages Total Working Days OR Total Working Hours Per Month

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -13,39 +13,22 @@ namespace EmployeeWageComputation
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 10;
         //variables
         int empHrs = 0;
-        int empWage = 0;
+        int totalEmpHrs = 0;  
         int totalEmpWage = 0;
+        int totalWorkingDays = 0;
         //Methods
         public void  MonthlyWage()  
         {
-            //UC 1: Check Employee Is Present Or Absent.
-            //UC 2: Calculate Daily Employee Wage.
-            //UC 3: Add Part Time Employee & Wage.
-            //UC 4: Solving Using Switch Case Statement.
-            //UC 5: Calculating Wages for a Month.
+            //UC 6:Calculating wages till number of working days or total working hours per  month is reached.
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0,3);
-            //if (empCheck == IS_FULL_TIME)
-            //{
-            //    Console.WriteLine("Full Time Employee is Present");
-            //    empHrs = 8;
-            //}
-            //else if(empCheck == IS_PART_TIME)
-            //{
-            //    Console.WriteLine("Part Time Employee is Present");
-            //    empHrs = 4;
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Employee is Absent");
-            //    empHrs = 0;
-            //}
-
-            for(int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while(totalEmpWage <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 switch (empCheck)
                 {
                     case IS_FULL_TIME:
@@ -60,12 +43,12 @@ namespace EmployeeWageComputation
 
                 }
                 //formula for Employee Wage Calculation.
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("Employee Wage is : " + empWage);
-                totalEmpWage += empWage;
+                totalEmpHrs+= empHrs;
+                Console.WriteLine("Day# : " + totalWorkingDays  + " Emp Hrs : " + empHrs);
+
             }
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage is : " + totalEmpWage);
-          
         }
     }
 }
