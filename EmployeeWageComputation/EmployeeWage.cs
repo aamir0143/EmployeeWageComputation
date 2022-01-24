4d6a6537ae2279a3e6199de70b886c75b46f9004
aamir0143 <mdaamirreza00@gmail.com>
2022-01-24 20:41:20 +0530

    [Aamir3] Add : Added Check the Attendence

---
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeWage
+    {
+        // Constants
+        int IS_FULL_TIME = 1;
+        //Methods
+        public void Attendence()
+        {
+            //UC 1: Check Employee Is Present Or Absent.
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Problems....");
+            EmployeeWage employee = new EmployeeWage();
+            employee.Attendence();
+            Console.ReadLine();
+        }
+    }
+}
