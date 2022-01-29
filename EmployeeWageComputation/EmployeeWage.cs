051f97ed49a58fd18ccdbb608f275fccd82da3fc
aamir0143 <mdaamirreza00@gmail.com>
2022-01-29 21:09:45 +0530

    [Aamir3] Add : Added Calculate total EmpWage till a condition of total working hours or days is reached for a month

--- EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWage.cs
@@ -8,12 +8,13 @@ namespace EmployeeWageComputation
 {
     public class EmployeeWage
     {
+        //UC 6:Calculating wages till number of working days or total working hours per  month is reached.
         // Constants
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 10;
+        public const int MAX_HRS_IN_MONTH = 100;
         //variables
         int empHrs = 0;
         int totalEmpHrs = 0;  
@@ -22,13 +23,12 @@ namespace EmployeeWageComputation
         //Methods
         public void  MonthlyWage()  
         {
-            //UC 6:Calculating wages till number of working days or total working hours per  month is reached.
-            while(totalEmpWage <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 //Computation.
                 int empCheck = random.Next(0, 3);
-                totalWorkingDays++;
                 switch (empCheck)
                 {
                     case IS_FULL_TIME:
